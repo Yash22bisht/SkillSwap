88ac425c1636c1b1dba744d585160c4c4e6cfd5a
Yash Bisht <yashbisht22.05@gmail.com>
2024-12-24 16:44:51 +0530

    modified

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import ChatIcon from "../assets/ChatIcon.png";
 const Navbar = () => {
   return (
     <div>
-      <nav className='w-screen h-24 bg-teal-600 flex items-center px-8 justify-between'>
+      <nav className='w-screen h-24 bg-teal-600 flex items-center px-8 justify-between '>
 
         <div className="logo flex">
           <a href="/"><img src="src\assets\swapLogo.png" alt="" className='bg-white rounded-full w-16 p-2' /></a>
@@ -18,7 +18,7 @@ const Navbar = () => {
           <a href="/" className=' ease-in hover:underline-offset-4 hover:text-shadow-md hover:underline'><h3>Contact</h3></a>
         </div>
 
-        <div className="myAccount flex items-center gap-8 mr-5">
+        <div className="myAccount flex items-center gap-8 mr-5 hidden">
           <div className="notification relative bottom-2">
             <div className="number relative top-4 left-5 bg-red-600 px-0 py-1 m-0 text-center rounded-[50%] font-bold  text-white text-sm">4</div>
             <img src={notificationIcon} alt="" className="w-8 h-8 cursor-pointer" />
@@ -27,13 +27,13 @@ const Navbar = () => {
             <div className="number relative top-4 left-5 bg-red-600 px-0 py-1 m-0 text-center rounded-[50%] font-bold  text-white text-sm ">1</div>
             <img src={ChatIcon} alt="" className="w-8 h-8 cursor-pointer" />
           </div>
-          <img src={ProfileIcon} alt="" className="w-12 cursor-pointer" />
+          <a href="/profile"><img src={ProfileIcon} alt="" className="w-12 cursor-pointer" /></a>
 
         </div>
-        {/* <div className="logs " >
-          <a href="/login"><button className='logButton  text-teal-600 hover:text-black  text-lg mx-2 bg-white  font-bold rounded-lg px-3 py-2 '>Login</button></a>
-          <a href="/register"><button className='logButton  text-teal-600 hover:text-black  text-lg mx-2 g-white  font-bold rounded-lg px-3 py-2 '>Get Started</button></a>
-        </div> */}
+        <div className="logs " >
+          <a href="/login"><button className='logButton  text-teal-600 hover:text-teal-800 hover:shadow-md hover:shadow-white  text-lg mx-2 bg-white  font-bold rounded-lg px-3 py-2 '>Login</button></a>
+          <a href="/register"><button className='logButton  text-teal-600 hover:text-teal-800 hover:shadow-md hover:shadow-white  text-lg mx-2 g-white bg-white font-bold rounded-lg px-3 py-2 '>Get Started</button></a>
+        </div>
 
       </nav>
     </div>
