ba44214a89bbc87fadc01126bd11c4e6663a3b57
Yash Bisht <yashbisht22.05@gmail.com>
2024-12-24 16:44:13 +0530

    updated Postskill

---
+++ src/pages/PostedSkill.jsx
@@ -0,0 +1,16 @@
+import Navbar from "../components/navbar"
+
+
+const PostedSkill = () => {
+    return (
+        <div className="postedSkills">
+            <Navbar />
+            <div className="container px-12">
+                <h1 className="heading text-4xl font-bold text-orange-500 mt-12 ">Website Devlopment</h1>
+                
+            </div>
+        </div>
+    )
+}
+
+export default PostedSkill
